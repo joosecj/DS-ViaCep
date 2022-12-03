25ef59245ac4a7ab7d11710c5a163b15824d6617
Jose Carlos <josecarloscjj@gmail.com>
2022-12-03 07:37:31 -0300

    Tratando input Number form

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,8 +1,51 @@
+import Address from "../models/address.js";
+
+
+function State() {
+    this.address = new Address();
+    this.btnSave = null;
+    this.btnClear = null;
+
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+
+    this.errorCep = null;
+    this.ErrorNumber = null;
+}
+
+const state = new State();
 
 export function init() {
 
-    const inputCep = document.forms.newAddress.cep;
+    state.inputCep = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.inputCity = document.forms.newAddress.city;
 
-    console.log(inputCep);
+    state.btnSave = document.forms.newAddress.btnSave;
+    state.btnClear = document.forms.newAddress.btnClear;
 
+    state.errorCep = document.querySelector('[data-error="cep"]');
+
+    state.ErrorNumber = document.querySelector('[data-error="number"]');
+
+    console.log(state);
+
+
+    state.inputNumber.addEventListener('change', handleInputNumberChange);
+}
+
+function handleInputNumberChange(event) {
+    if (event.target.value == "") {
+        setFormError("number", "Campo requerido");
+    } else {
+        setFormError("number", "");
+    }
+}
+
+function setFormError(key, value) {
+    const element = document.querySelector(`[data-error="${key}"]`);
+    element.innerHTML = value;
 }
